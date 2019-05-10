2f7f27423c1ea71036c8e37e7def99713cb3c331
onahbosen <onahbosen@gmail.com>
2019-05-10 11:24:18 -0500

    commit

--- front-end-shit/assets/js/app.js
+++ front-end-shit/assets/js/app.js
@@ -5,7 +5,8 @@ window.chartColors = {
     orange: 'rgb(255, 159, 64)',
     yellow: 'rgb(255, 205, 86)',
     green: 'rgb(75, 192, 192)',
-    blue: 'rgb(54, 162, 235)',
+    lightBlue: 'rgb(183, 220, 244)',
+    darkBlue: 'rgb(86, 108, 122)',
     purple: 'rgb(153, 102, 255)',
     grey: 'rgb(231,233,237)'
 };
@@ -16,8 +17,8 @@ var config = {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
             label: 'My First dataset',
-            backgroundColor: window.chartColors.red,
-            borderColor: window.chartColors.red,
+            backgroundColor: window.chartColors.lightBlue,
+            borderColor: window.chartColors.lightBlue,
             data: [
                 1,
                 -2,
@@ -31,8 +32,8 @@ var config = {
         }, {
             label: 'My Second dataset',
             fill: false,
-            backgroundColor: window.chartColors.blue,
-            borderColor: window.chartColors.blue,
+            backgroundColor: window.chartColors.darkBlue,
+            borderColor: window.chartColors.darkBlue,
             data: [
                 8,
                 -9,
@@ -94,4 +95,13 @@ $(document).ready(function () {
         console.log(selectedVal);
         console.log(selectedText);
     });
+    $('.chart-div').hide();
+    $('.chart-toggle').hide();
+    $('.table-div').hide();
+    $('.submit').on('click', function(){
+        $('.form-div').hide();
+        $('.chart-div').show();
+        $('.chart-toggle').show();
+        $('.table-div').show();
+    })
 });
